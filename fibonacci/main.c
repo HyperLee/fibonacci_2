cb57a68d9b96ea4653796825c9a59f1d37b4e169
Peter.Chiu <tidub76217@hotmail.com>
2022-02-15 22:15:49 +0800

    Update main.c
    fibonacci c

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -2,13 +2,41 @@
 //  main.c
 //  fibonacci
 //
-//  Created by 邱字立 on 2022/2/15.
+//  Created by HyperLee on 2022/2/15.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    int result;
+    int number;
+    //int i;
+    
+    printf("Enter yout number: ");
+    scanf("%d", &number);
+    
+    result = fibonacci(number);
+    
+    
+    printf("Fibonacci(%1d) = %1d\n", number, result);
+    
+    
     return 0;
 }
+
+
+int fibonacci(int n)
+{
+    if(n ==0 || n ==1)
+    {
+        return n;
+    }
+    else{
+        return fibonacci(n - 1 ) + fibonacci(n - 2);
+    }
+}
+
