d7f25e935738fcea49db940d9044b358116361fa
Peter.Chiu <tidub76217@hotmail.com>
2022-02-15 22:13:09 +0800

    Initial Commit

---
+++ fibonacci/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fibonacci
+//
+//  Created by 邱字立 on 2022/2/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
